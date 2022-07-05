2685f9cd46aafb393c926316617cc63f90b425e7
Alastor2189 <g.lucafarci89@gmail.com>
2022-07-05 10:28:04 +0200

    inversione elementi nel show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,19 +7,20 @@
                 <h4>
                     {{ $show_comic->title }}
                 </h4>
-                <p class="mb-4">{{ $show_comic->description }}</p>
-                    <div>
-                        <a class="btn btn-primary me-2"  href="{{ route('comics.edit', ['comic' => $show_comic->id]) }}">Modifica</a>
-
-                         <form action="{{ route('comics.destroy', [ 'comic' => $show_comic->id ]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger" onclick="return confirm('Do you confirm?')">Cancella</button>                         
-                        </form>
-                    </div>
                 <p>
                     <img src="{{ $show_comic->thumb}}" alt="">
                 </p>
+                <div>
+                    <form action="{{ route('comics.destroy', [ 'comic' => $show_comic->id ]) }}" method="POST">
+                       @csrf
+                       @method('DELETE')
+                       <button type="submit" class="btn btn-danger" onclick="return confirm('Do you confirm?')">Cancella</button>
+                       <a class="btn btn-primary me-2"  href="{{ route('comics.edit', ['comic' => $show_comic->id]) }}">Modifica</a>                         
+                   </form>
+               </div>
+                <p class="mb-4">{{ $show_comic->description }}</p>
+                    
+                
                 <p>
                     {{ $show_comic->price}}
                 </p>
